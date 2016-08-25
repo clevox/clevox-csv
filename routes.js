328d5973caf827a9d6080aebc32cc2348f322c25
Shahjada <shahjada.talukdar@gmail.com>
2016-08-26 00:05:33 +0600

    Upload post

--- routes.js
+++ routes.js
@@ -3,4 +3,10 @@ module.exports = function (app) {
   app.get('/api/ping', function (req, res, next) {
     res.send("pong");
   });
+
+  var uploadCsvHandler = require('./app/handlers/upload-csv-handler');
+
+  app.post('/api/upload-csv', uploadCsvHandler.upload);
+
+
 };

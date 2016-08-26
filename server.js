a7779dadfd95a0a745fee8f2700b71b652c00ac5
Shahjada <shahjada.talukdar@gmail.com>
2016-08-26 16:34:40 +0600

    cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
 
+
 var port = 5001;
+
+app.use(cors());
 
 require('./routes')(app);
 

cd738ad58604e448d8e1abcf41635e0d65376122
Shahjada <shahjada.talukdar@gmail.com>
2016-08-26 16:33:03 +0600

    formidable implementation

--- app/handlers/upload-csv-handler.js
+++ app/handlers/upload-csv-handler.js
@@ -1,7 +1,29 @@
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+var uploadDir = path.join(path.dirname(require.main.filename), '/uploads');
+
 var upload = function (req, res) {
 
+  var form = new formidable.IncomingForm();
+  
+  form.multiples = true;
+  form.uploadDir = uploadDir;
+  console.log(uploadDir);
 
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
 
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  form.on('end', function() {
+   return res.end('success');
+  });
+
+  form.parse(req);
 };
 
 
